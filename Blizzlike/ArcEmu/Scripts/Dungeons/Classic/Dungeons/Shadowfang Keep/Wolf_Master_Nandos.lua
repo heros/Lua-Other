c9c4c8cebe4143e7a9d9ca54a2a8fe6c7b16183f
zdroid9770 <zdroid9770@yahoo.com>
2012-09-30 03:39:12 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Shadowfang Keep/Wolf_Master_Nandos.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Shadowfang Keep/Wolf_Master_Nandos.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Shadowfang Keep/Wolf_Master_Nandos.lua
@@ -18,29 +18,19 @@
 -- % Complete: 1%
 -- Comments: Only ChatMessages
 
--- [[ Spells ]] --
-
-
-
 -- [[ Functions ]] --
 function Nandos_OnCombat(Unit, Event)
-	unit:SendChatMessage(14, 0, "I can't believe it! You've destroyed my pack... Now face my wrath!")
+	Unit:SendChatMessage(14, 0, "I can't believe it! You've destroyed my pack... Now face my wrath!")
 end
 
 function Nandos_OnLeaveCombat(Unit, Event)
-	unit:RemoveEvents()
-end
-
-function Nandos_OnKilledTarget(Unit, Event)
+	Unit:RemoveEvents()
 end
 
 function Nandos_OnDied(Unit, Event)
-	unit:RemoveEvents()
+	Unit:RemoveEvents()
 end 
 
-
--- [[ Registering ]] --
 RegisterUnitEvent(3927, 1, "Nandos_OnCombat")
 RegisterUnitEvent(3927, 2, "Nandos_OnLeaveCombat")
-RegisterUnitEvent(3927, 3, "Nandos_OnKilledTarget")
 RegisterUnitEvent(3927, 4, "Nandos_OnDied")
