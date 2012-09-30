1fdb7f5504f8826fb3b71f8d931e12c0fc1aba2f
zdroid9770 <zdroid9770@yahoo.com>
2012-09-30 02:04:41 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Scarlet Monastery/Graveyard/Bloodmage Thalnos.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Scarlet Monastery/Graveyard/Bloodmage Thalnos.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Scarlet Monastery/Graveyard/Bloodmage Thalnos.lua
@@ -1,4 +1,9 @@
--- Bloodmage Thalnos
+-------------------------------------------------------------------
+-- This script is created by zdroid9770; please do not edit this --
+-- script and claim it as your own, as of All rights are claimed --
+-- by me.                                                        --
+--                     Copyright© zdroid9770			 --
+-------------------------------------------------------------------
 
 function Thalnos_Shadowbolt(pUnit)
 local target = pUnit:GetMainTank()
