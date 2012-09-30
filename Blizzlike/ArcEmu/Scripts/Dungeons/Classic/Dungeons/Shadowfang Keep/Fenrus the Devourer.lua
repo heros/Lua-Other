4621480d06becd8ccd50e5b875fbac7e65b6bf4a
zdroid9770 <zdroid9770@yahoo.com>
2012-09-30 03:33:39 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Shadowfang Keep/Fenrus the Devourer.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Shadowfang Keep/Fenrus the Devourer.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Shadowfang Keep/Fenrus the Devourer.lua
@@ -17,40 +17,39 @@
 ========================================]]--
 
 function Puppy_Chill(Unit, Player, Event)
-Unit:CastSpell(21098)
+ Unit:CastSpell(21098)
 end
 
 function Puppy_Volly(Unit, Player, Event)
-Unit:CastSpell(38837)
+	Unit:CastSpell(38837)
 end
 
 function Puppy_Armor(Unit, Player, Event)
-Unit:CastSpell(27134)
+	Unit:CastSpell(27134)
 end
 
 function Puppy_IceNova(Unit, Player, Event)
-Unit:CastSpell(31250)
+	Unit:CastSpell(31250)
 end
 
 function Puppy_OnCombat(Unit, Player, Event)
-Unit:RegisterEvent("Puppy_Chill",80000, 0)
-Unit:RegisterEvent("Puppy_Volly",40000, 0)
-Unit:RegisterEvent("Puppy_Armor",20000, 0)
-Unit:RegisterEvent("Puppy_IceNova",90000, 0)
+	Unit:RegisterEvent("Puppy_Chill",80000, 0)
+	Unit:RegisterEvent("Puppy_Volly",40000, 0)
+	Unit:RegisterEvent("Puppy_Armor",20000, 0)
+	Unit:RegisterEvent("Puppy_IceNova",90000, 0)
 end
 
-
 function Puppy_OnLeaveCombat(Unit, Player, Event)
-Unit:RemoveEvents()
+	Unit:RemoveEvents()
 end
 
 function Puppy_OnKilledTarget(Unit, Player, Event)
-Unit:CastSpell(37669)
-Unit:RemoveEvents()
+	Unit:CastSpell(37669)
+	Unit:RemoveEvents()
 end
 
 function Puppy_OnDied(Unit, Player, Event)
-Unit:RemoveEvents()
+	Unit:RemoveEvents()
 end
 
 RegisterUnitEvent(4274, 1, "Puppy_OnCombat")
