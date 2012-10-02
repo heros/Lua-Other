d5de797897a75cc8292a8b4d0f1b9dd3172ca391
zdroid9770 <zdroid9770@yahoo.com>
2012-10-02 05:26:23 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Zul'Farrak/GuardianKidlic.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Zul'Farrak/GuardianKidlic.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Dungeons/Zul'Farrak/GuardianKidlic.lua
@@ -17,66 +17,64 @@
 
 phase = 0
 
-function PhaseCheck(unit, event, miscunit, misc)
-	if unit:GetHealthPct() <= 60 and phase == 0 then
+function PhaseCheck(Unit, event, miscUnit, misc)
+	if ((Unit:GetHealthPct() <= 60) and (phase == 0)) then
 		phase = 1
-		unitRegisterEvent("GuardianKidlic_Phase1", 3000, 1)
-	elseif unit:GetHealthPct() <= 40 and phase == 1 then
+		UnitRegisterEvent("GuardianKidlic_Phase1", 3000, 1)
+	elseif ((Unit:GetHealthPct() <= 40) and (phase == 1)) then
 		phase = 2
-		unit:RegisterEvent("GuardianKidlic_Phase2", 3000, 1)
+		Unit:RegisterEvent("GuardianKidlic_Phase2", 3000, 1)
 	end
 end
 
 -- phase 1
 
-function GuardianKidlic_MortalWound(unit, event, miscunit, misc)
-	unit:FullCastSpellOnTarget(28467, GetMainTank())
+function GuardianKidlic_MortalWound(Unit, event, miscUnit, misc)
+	Unit:FullCastSpellOnTarget(28467, GetMainTank())
 end
 
-function GuardianKidlic_EnragingStrike(unit, event, miscunit, misc)
-	unit:FullCastSpellOnTarget(37974, GetClosestPlayer())
+function GuardianKidlic_EnragingStrike(Unit, event, miscUnit, misc)
+	Unit:FullCastSpellOnTarget(37974, GetClosestPlayer())
 end
 
-function GuardianKidlic_Phase1(unit, event, miscunit, misc)
-	unit:RemoveEvents()
-	unit:SendChatMessage(12, 0, "You wont succeed!")
-	unit:RegisterEvent("PhaseCheck", 3000, 0)
-	unit:RegisterEvent("GuardianKidlic_MortalWound", 8000, 0)
-	unit:RegisterEvent("GuardianKidlic_EnragingStrike", 5000, 0)
+function GuardianKidlic_Phase1(Unit, event, miscUnit, misc)
+	Unit:RemoveEvents()
+	Unit:SendChatMessage(12, 0, "You wont succeed!")
+	Unit:RegisterEvent("PhaseCheck", 3000, 0)
+	Unit:RegisterEvent("GuardianKidlic_MortalWound", 8000, 0)
+	Unit:RegisterEvent("GuardianKidlic_EnragingStrike", 5000, 0)
 end
 
 -- phase 2
 
-function GuardianKidlic_MightyBlow(unit, event, miscunit, misc)
-	unit:FullCastSpellOnTarget(43673, GetClosestPlayer())
-	unit:ClearThreadList()
+function GuardianKidlic_MightyBlow(Unit, event, miscUnit, misc)
+	Unit:FullCastSpellOnTarget(43673, GetClosestPlayer())
+	Unit:ClearThreadList()
 end
 
-function GuardianKidlic_Phase2(unit, event, miscunit, misc)
-	unit:RemoveEvents()
-	unit:SendChatMessage(12, 0, "Lord! Give me strengh!")
-	unit:RegisterEvent("GuardianKidlic_MortalWound", 8000, 0)
-	unit:RegisterEvent("GuardianKidlic_EnragingStrike", 5000, 0)
-	unit:RegisterEvent("GuardianKidlic_MightyBlow", 10000, 0)
+function GuardianKidlic_Phase2(Unit, event, miscUnit, misc)
+	Unit:RemoveEvents()
+	Unit:SendChatMessage(12, 0, "Lord! Give me strengh!")
+	Unit:RegisterEvent("GuardianKidlic_MortalWound", 8000, 0)
+	Unit:RegisterEvent("GuardianKidlic_EnragingStrike", 5000, 0)
+	Unit:RegisterEvent("GuardianKidlic_MightyBlow", 10000, 0)
 end
 
-function GuardianKidlic_OnEnterCombat(unit, event, miscunit, misc)
-	unit:SendChatMessage(12, 0, "I will crush you!")
-	unit:RegisterEvent("PhaseCheck", 3000, 0)
+function GuardianKidlic_OnEnterCombat(Unit, event, miscUnit, misc)
+	Unit:SendChatMessage(12, 0, "I will crush you!")
+	Unit:RegisterEvent("PhaseCheck", 3000, 0)
 end
 
-RegisterUnitEvent(230070, 1, "GuardianKidlic_OnEnterCombat")
-
-function GuardianKidlic_OnWipe(unit, event, miscunit, misc)
-	unit:RemoveEvents()
-	unit:SendChatMessage(12, 0, "As i said...")
+function GuardianKidlic_OnWipe(Unit, event, miscUnit, misc)
+	Unit:RemoveEvents()
+	Unit:SendChatMessage(12, 0, "As i said...")
 end
 
-RegisterUnitEvent(230070, 2, "GuardianKidlic_OnWipe")
-
-function GuardianKidlic_Kill(unit, event, miscunit, misc)
-	unit:RemoveEvents()
-	unit:SendChatMessage(12, 0, "Noooooo!")
+function GuardianKidlic_Kill(Unit, event, miscUnit, misc)
+	Unit:RemoveEvents()
+	Unit:SendChatMessage(12, 0, "Noooooo!")
 end
 
 RegisterUnitEvent(230070, 4, "GuardianKidlic_Kill")
+RegisterUnitEvent(230070, 2, "GuardianKidlic_OnWipe")
+RegisterUnitEvent(230070, 1, "GuardianKidlic_OnEnterCombat")
