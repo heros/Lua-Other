d468a535579cb822651ed3332db265a0efa1ec9c
zdroid9770 <zdroid9770@yahoo.com>
2012-10-14 02:49:42 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Ahn'Qiraj/Temple of Ahn'Qiraj/Battleguard Sartura.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Ahn'Qiraj/Temple of Ahn'Qiraj/Battleguard Sartura.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Ahn'Qiraj/Temple of Ahn'Qiraj/Battleguard Sartura.lua
@@ -24,14 +24,14 @@ Battleguard Sartura yells: You will be judged for defiling these sacred grounds!
 
 function Sartura_Knockback(pUnit, event)
 	pUnit:CastSpellOnTarget(10689, pUnit:GetRandomPlayer(0))
-	local kbtimer=math.random(25000, 60000)
-	pUnit:RegisterEvent("Sartura_Knockback2",kbtimer,1)
+	local kbtimer = math.random(25000, 60000)
+	pUnit:RegisterEvent("Sartura_Knockback2", kbtimer, 1)
 end
 
 function Sartura_Knockback2(pUnit, event)
 	pUnit:CastSpellOnTarget(10689, pUnit:GetRandomPlayer(0))
-	local kbtimer=math.random(25000, 60000)
-	pUnit:RegisterEvent("Sartura_Knockback",kbtimer,1)
+	local kbtimer = math.random(25000, 60000)
+	pUnit:RegisterEvent("Sartura_Knockback", kbtimer, 1)
 end
 
 function Sartura_Whirlwind(Unit)
@@ -39,10 +39,10 @@ function Sartura_Whirlwind(Unit)
 end
 
 function Sartura_Phase2(Unit, event)
-	if Unit:GetHealthPct() < 20 then
+	if (Unit:GetHealthPct() < 20) then
 		Unit:RemoveEvents()
 		Unit:CastSpell(28747)
-		Unit:RegisterEvent("Sartura_Enrage",1000,0)
+		Unit:RegisterEvent("Sartura_Enrage", 1000, 0)
 	end
 end
 
@@ -58,14 +58,14 @@ function Sartura_Enrage(Unit)
 end
 
 function Sartura_OnCombat(Unit, event)
-	setvars(Unit, {EnrageTimer=1});
+	setvars(Unit, {EnrageTimer = 1});
 	local kbtimer=math.random(25000, 60000)
 	Unit:PlaySoundToSet(8646)
-	Unit:SendChatMessage(12, 0,"You will be judged for defiling these sacred grounds! The laws of the Ancients will not be challenged! Trespassers will be annihilated!")
+	Unit:SendChatMessage(12, 0, "You will be judged for defiling these sacred grounds! The laws of the Ancients will not be challenged! Trespassers will be annihilated!")
 	Unit:RegisterEvent("Sartura_Whirlwind", 30000, 0)
 	Unit:RegisterEvent("Sartura_Phase2", 1000, 0)
 	Unit:RegisterEvent("Sartura_Knockback", kbtimer, 1)
-	Unit:RegisterEvent("Sartura_Enrage",1000,0)
+	Unit:RegisterEvent("Sartura_Enrage", 1000, 0)
 end
 
 function Sartura_OnLeaveCombat(Unit, event)
@@ -74,16 +74,16 @@ end
 
 function Sartura_OnKilledTarget(Unit, event)
 	Unit:PlaySoundToSet(8647)
-	Unit:SendChatMessage(12, 0,"I sentence you to death!")
+	Unit:SendChatMessage(12, 0, "I sentence you to death!")
 end
 
 function Sartura_OnDied(Unit, event)
 	Unit:PlaySoundToSet(8648)
-	Unit:SendChatMessage(12, 0,"I serve to the last!")
+	Unit:SendChatMessage(12, 0, "I serve to the last!")
 	Unit:RemoveEvents()
 end
 
-RegisterUnitEvent(15516,1,"Sartura_OnCombat")
-RegisterUnitEvent(15516,2,"Sartura_OnLeaveCombat")
-RegisterUnitEvent(15516,3,"Sartura_OnKilledTarget")
-RegisterUnitEvent(15516,4,"Sartura_OnDied")
+RegisterUnitEvent(15516, 1, "Sartura_OnCombat")
+RegisterUnitEvent(15516, 2, "Sartura_OnLeaveCombat")
+RegisterUnitEvent(15516, 3, "Sartura_OnKilledTarget")
+RegisterUnitEvent(15516, 4, "Sartura_OnDied")
