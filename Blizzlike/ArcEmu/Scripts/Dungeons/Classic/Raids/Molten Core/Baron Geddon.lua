421b4360670b133e5a2b89a615b1e0ef6b37c718
zdroid9770 <zdroid9770@yahoo.com>
2012-10-14 03:20:35 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Baron Geddon.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Baron Geddon.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Baron Geddon.lua
@@ -15,32 +15,33 @@ redistributing and/or using this software. Thank you.
 ~~End of License Agreement
 -- LUA++ staff, March 26, 2008. ]]
 
-
-
 function BGeddon_OnCombat(Unit,event)
 	Unit:CastSpell(19659)
-	Unit:RegisterEvent("BGeddon_Inferno",math.random(30000,45000), 0)
-	Unit:RegisterEvent("BGeddon_IgniteMana",math.random(22000,35000), 0)
-	Unit:RegisterEvent("BGeddon_LivingBomb",math.random(46000,50000), 0)
+	Unit:RegisterEvent("BGeddon_Inferno", math.random(30000, 45000), 0)
+	Unit:RegisterEvent("BGeddon_IgniteMana", math.random(22000, 35000), 0)
+	Unit:RegisterEvent("BGeddon_LivingBomb", math.random(46000, 50000), 0)
 	Unit:RegisterEvent("BGeddon_Amargeddon", 1000, 0)
 end
+
 function BGeddon_OnWipe(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function BGeddon_OnDied(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function BGeddon_Inferno(Unit)
 	Unit:FullCastSpell(19695)
 end
+
 function BGeddon_IgniteMana(Unit)
 	Unit:CastSpell(19659)
 end
 
 function BGeddon_LivingBombBoom(Unit,event)
 	local args=getvars(Unit)
-	if args.livingbombtarget:HasAura(20475) == true then
-	print "Player is the bomb, getting ready to go boom!"
+	if (args.livingbombtarget:HasAura(20475) == true) then
 		Unit:SpawnCreature(70100, args.livingbombtarget:GetX(), args.livingbombtarget:GetY(), args.livingbombtarget:GetZ(), 90, 14, 0)
 		args.livingbombtarget:Unroot()
 	end
@@ -49,14 +50,14 @@ end
 function BGeddon_LivingBomb(Unit)
 	setvars(Unit, {livingbombtarget = Unit:GetRandomPlayer(0)})
 	local args=getvars(Unit)
-	if args.livingbombtarget ~= nil then
+	if (args.livingbombtarget ~= nil) then
 		Unit:FullCastSpellOnTarget(20475, args.livingbombtarget)
 		args.livingbombtarget:Root()
 		Unit:RegisterEvent("BGeddon_LivingBombBoom", 7900, 1)
 	end
 end
 function BGeddon_Amargeddon(Unit,event)
-	if Unit:GetHealthPct() == 2 then
+	if (Unit:GetHealthPct() == 2) then
 		Unit:StopMovement(8000)
 		Unit:FullCastSpell(20478)
 	end
