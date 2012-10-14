565b4a8676934fceb1f4b92c868a97deedaacf37
zdroid9770 <zdroid9770@yahoo.com>
2012-10-14 03:25:44 -0700

    Update Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Gehennas.lua

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Gehennas.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Gehennas.lua
@@ -25,41 +25,41 @@ function Gehennas_OnCombat(Unit,event)
 	local tbl = Unit:GetInRangeFriends()
 	local mt = Unit:GetClosestPlayer()
 	for k,v in pairs(tbl) do
-		if v:GetEntry() == 11664 then
-		v:ModifyWalkSpeed(8)
-		v:MoveTo(mt:GetX(), mt:GetY(), mt:GetZ(), mt:GetO())
+		if (v:GetEntry() == 11664) then
+			v:ModifyWalkSpeed(8)
+			v:MoveTo(mt:GetX(), mt:GetY(), mt:GetZ(), mt:GetO())
 		end
 	end
 end
+
 function Gehennas_OnDied(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function Gehennas_OnWipe(Unit,event)
-	if Unit:IsAlive() == true then
-	Unit:SpawnCreature(11664, 905.372314, -542.778442, -202.930298, 3.989631)
-	Unit:SpawnCreature(11664, 890.958008, -539.140015, -203.050995, 5.445430)
-	Unit:RemoveEvents()
+	if (Unit:IsAlive() == true) then
+		Unit:SpawnCreature(11664, 905.372314, -542.778442, -202.930298, 3.989631)
+		Unit:SpawnCreature(11664, 890.958008, -539.140015, -203.050995, 5.445430)
+		Unit:RemoveEvents()
 	else
-	Unit:RemoveEvents()
+		Unit:RemoveEvents()
 	end
 end
 	
 function GehennasSpells(Unit,event)
 	local rand = math.random(1,3)
 	local plr = Unit:GetRandomPlayer(0)
-	if rand == 1 and Unit:GetInRangePlayersCount() > 1 then
-		local notmaintank=Unit:GetRandomPlayer(0)
-		if notmaintank ~= Unit:GetMainTank() then
-		Unit:FullCastSpellOnTarget(19729,notmaintank)
+	if (rand == 1) and (Unit:GetInRangePlayersCount() > 1) then
+		if (Unit:GetRandomPlayer(0) ~= Unit:GetMainTank()) then
+			Unit:FullCastSpellOnTarget(19729, Unit:GetRandomPlayer(0))
 		else
-		repeat
-		local notmaintank=Unit:GetRandomPlayer(0)
-		until notmaintank ~= Unit:GetMainTank()
-		Unit:FullCastSpellOnTarget(19729, notmaintank)
+			repeat
+			until Unit:GetRandomPlayer(0) ~= Unit:GetMainTank()
+			Unit:FullCastSpellOnTarget(19729, Unit:GetRandomPlayer(0))
 		end
-	elseif rand == 2 and plr ~= nil then
+	elseif (rand == 2) and (plr ~= nil) then
 		Unit:CastSpellAoF(plr:GetX(),plr:GetY(),plr:GetZ(),19717)
-	elseif rand == 3 then
+	elseif (rand == 3) then
 		Unit:CastSpell(19716)
 	end
 end
@@ -77,39 +77,42 @@ function GehennasElites_OnCombat(Unit,event)
 		local tbl = Unit:GetInRangeFriends()
 		local mt = Unit:GetClosestPlayer()
 		for k,v in pairs(tbl) do
-			if v:GetEntry() == 11664 then
-				if v:IsCreatureMoving() == false then
-				v:ModifyWalkSpeed(8)
-				v:MoveTo(mt:GetX(), mt:GetY(), mt:GetZ(), mt:GetO())
+			if (v:GetEntry() == 11664) then
+				if (v:IsCreatureMoving() == false) then
+					v:ModifyWalkSpeed(8)
+					v:MoveTo(mt:GetX(), mt:GetY(), mt:GetZ(), mt:GetO())
 				end
 			end
 		end
 		for k,v in pairs(tbl) do
-			if v:GetEntry() == 12259 then
-				if v:IsCreatureMoving() == false then
-				v:ModifyWalkSpeed(8)
-				v:MoveTo(mt:GetX(), mt:GetY(), mt:GetZ(), mt:GetO())
+			if (v:GetEntry() == 12259) then
+				if (v:IsCreatureMoving() == false) then
+					v:ModifyWalkSpeed(8)
+					v:MoveTo(mt:GetX(), mt:GetY(), mt:GetZ(), mt:GetO())
 				end
 			end
 		end
-		Unit:RegisterEvent("GehennasElitesSpells", math.random(12000, 18000), 0)
+	Unit:RegisterEvent("GehennasElitesSpells", math.random(12000, 18000), 0)
 end
+
 function GehennasElites_OnWipe(Unit,event)
 	Unit:RemoveEvents()
 	Unit:Despawn(1000, 0)
 end
+
 function GehennasElites_OnDied(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function GehennasElitesSpells(Unit,event)
 	local rand = math.random(1,3)
 	local tank = Unit:GetMainTank()
-	if rand == 1 then
+	if (rand == 1) then
 		Unit:CastSpell(20277)
-	elseif rand == 2 and tank ~= nil then
-		Unit:FullCastSpellOnTarget(24317,tank)
-	elseif rand == 3 and tank ~= nil then
-		Unit:FullCastSpellOnTarget(19730,tank)
+	elseif (rand == 2) and (tank ~= nil) then
+		Unit:FullCastSpellOnTarget(24317, tank)
+	elseif (rand == 3) and (tank ~= nil) then
+		Unit:FullCastSpellOnTarget(19730, tank)
 	end
 end
 	
