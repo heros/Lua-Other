b4bf4b865426ae2b53153613f1c5e0bb8a00d66b
zdroid9770 <zdroid9770@yahoo.com>
2012-10-14 03:30:34 -0700

    Fixed: Wrong places for Register

--- Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Lucifron.lua
+++ Blizzlike/ArcEmu/Scripts/Dungeons/Classic/Raids/Molten Core/Lucifron.lua
@@ -14,24 +14,24 @@ redistributing and/or using this software. Thank you.
 
 ~~End of License Agreement
 -- LUA++ staff, March 26, 2008. ]]
-RegisterUnitEvent(12118,1,"Lucifron_OnCombat")
-RegisterUnitEvent(12118,4,"Lucifron_OnDeath")
-RegisterUnitEvent(12118,2,"Lucifron_OnWipe")
 --[[
 	LUCIFRON's AI
 ]]
 function Lucifron_OnCombat(Unit,event)
-	Unit:RegisterEvent("LucifronSpells",math.random(18000,21000),0)
+	Unit:RegisterEvent("LucifronSpells", math.random(18000, 21000), 0)
 	Unit:RegisterEvent("ShadowShock", 7000, 0)
 end
+
 function Lucifron_OnDeath(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function Lucifron_OnWipe(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function LucifronSpells(Unit,event)
-	if math.random(0,1) < 0.5 then
+	if (math.random(0,1) < 0.5) then
 		Unit:CastSpell(19702)
 	else
 		Unit:CastSpell(19703)
@@ -41,22 +41,32 @@ end
 function ShadowShock(Unit,event)
 	Unit:FullCastSpellOnTarget(19460,Unit:GetRandomPlayer(0))
 end
+
+RegisterUnitEvent(12118,1,"Lucifron_OnCombat")
+RegisterUnitEvent(12118,4,"Lucifron_OnDeath")
+RegisterUnitEvent(12118,2,"Lucifron_OnWipe")
+
 --[[
 	FLAMEWALKER's AI
 ]]
-RegisterUnitEvent(12119,1,"LucifronProtector_OnCombat")
-RegisterUnitEvent(12119,2,"LucifronProtector_OnWipe")
-RegisterUnitEvent(12119,4,"LucifronProtector_OnDied")
+
 function LucifronProtector_OnCombat(Unit,event)
 	Unit:RegisterEvent("LucifronProtector_Cleave", math.random(12000, 16000), 1)
 end
+
 function LucifronProtector_Cleave(Unit,event)
-		Unit:FullCastSpellOnTarget(39047, Unit:GetMainTank())
-		Unit:RegisterEvent("LucifronProtector_Cleave", math.random(12000, 16000), 0)
+	Unit:FullCastSpellOnTarget(39047, Unit:GetMainTank())
+	Unit:RegisterEvent("LucifronProtector_Cleave", math.random(12000, 16000), 0)
 end
+
 function LucifronProtector_OnDied(Unit,event)
 	Unit:RemoveEvents()
 end
+
 function LucifronProtector_OnWipe(Unit,event)
 	Unit:RemoveEvents()
 end
+
+RegisterUnitEvent(12119,1,"LucifronProtector_OnCombat")
+RegisterUnitEvent(12119,2,"LucifronProtector_OnWipe")
+RegisterUnitEvent(12119,4,"LucifronProtector_OnDied")
